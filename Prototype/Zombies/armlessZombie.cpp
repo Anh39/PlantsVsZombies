5f8e9155acfaec5d1678cbea0592d065cc228c29
18northside <trantrongnghia271204@gmail.com>
2025-04-14 00:04:01 +0700

    Update armlessZombie.cpp

--- Prototype/Zombies/armlessZombie.cpp
+++ Prototype/Zombies/armlessZombie.cpp
@@ -2,35 +2,12 @@
 
 ArmlessZombie::ArmlessZombie() {
     this->Name = "ArmlessZombie";
-
-    this->colorRect = new ColorRect();
-    this->colorRect->color = Color(255, 0, 0); 
-    this->AddChildren(this->colorRect);
-
-    this->collider = new CollideNode();
-    this->collider->collideFilter = CollideLayer1;
-    this->AddChildren(this->collider);
-
+    this->colorRect->color = Color(0, 255, 0); // Màu xanh lá
 }
 
 ArmlessZombie::~ArmlessZombie() {
-  
-
-void ArmlessZombie::SetSize(const Vector2& size) {
-  
-    this->colorRect->size = size;
-    this->collider->size = size;
 }
 
-ArmlessZombie* ArmlessZombie::Copy() {
-    
-    ArmlessZombie* newArmlessZombie = new ArmlessZombie();
-    newArmlessZombie->SetSize(this->colorRect->size);  // Sao chép kích thước
-    return newArmlessZombie;
-}
-
-void ArmlessZombie::Update(float delta) {
-
-  
-    this->position += Vector2(-10, 0) * delta;
+BaseZombie* ArmlessZombie::Copy() {
+    return new ArmlessZombie(*this);
 }
